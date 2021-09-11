72b2dc2ba4a9168ea7f2a3351619fc4c56c4cfb7
Mehmet Can Demir <mhmtcndmr01@gmail.com>
2021-09-11 21:58:16 +0300

    built class Agent to manage tasks

--- cubis/src/cubis.py
+++ cubis/src/cubis.py
@@ -4,26 +4,49 @@ import selenium
 import selenium.webdriver
 
 
+class Agent:
+    def __init__(self):
+        # read cfg
+        self.BASE_DIR = os.path.dirname(__file__) + "/../.."
+
+        with open(self.BASE_DIR + "/config.json", "r") as f:
+            self.cfg = json.load(f)
+
+        # TESTING <---------------------->
+        self.set_webdriver()
+        self.set_url(self.cfg["urls"]["login"])
+        self.login()
+        self.set_url(self.cfg["urls"]["obs"])
+        self.close()
+        # TESTING <---------------------->
+
+    def set_webdriver(self):    # todo: add chrome
+        print("<*> setting up driver..")
+        options = selenium.webdriver.FirefoxOptions()
+        options.set_headless(True)
+        self.driver = selenium.webdriver.Firefox(executable_path=self.BASE_DIR + "/cubis/webdrivers/geckodriver",
+                                                 options=options)
+        print("<*> driver is set!")
+
+    def set_url(self, url):
+        print("<*> conencting {}".format(url))
+        self.driver.get(url)
+        print("<*> connection set!")
+
+    def login(self):
+        print("<*> logging in..")
+        username_box = self.driver.find_element_by_id(self.cfg["login_page"]["input_username_id"])
+        password_box = self.driver.find_element_by_id(self.cfg["login_page"]["input_password_id"])
+        login_button = self.driver.find_element_by_id(self.cfg["login_page"]["button_login_id"])
+        username_box.send_keys(self.cfg["login_data"]["username"])
+        password_box.send_keys(self.cfg["login_data"]["password"])
+        login_button.click()
+        print("<*> logged in!")
+
+    def close(self):
+        print("<*> closing client..")
+        self.driver.close()
+
+
 def main():
-    # read cfg
-    with open(os.path.dirname(__file__) + "/../../config.json", "r") as f:
-        cfg = json.load(f)
-
-    # setup driver
-    print("setting up driver..")
-    driver = selenium.webdriver.Firefox(executable_path=os.path.dirname(__file__) + "/../webdrivers/geckodriver")
-
-    # go to login page and login
-    driver.get(cfg["urls"]["login"])
-
-    username_box = driver.find_element_by_id(cfg["login_page"]["input_username_id"])
-    password_box = driver.find_element_by_id(cfg["login_page"]["input_password_id"])
-    login_button = driver.find_element_by_id(cfg["login_page"]["button_login_id"])
-
-    username_box.send_keys(cfg["login_data"]["username"])
-    password_box.send_keys(cfg["login_data"]["password"])
-
-    login_button.click()
-
-    pass
-
+    agent = Agent()
