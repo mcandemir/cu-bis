1e6e867a47ff88dca5299b685eec91819f679269
Mehmet Can Demir <mhmtcndmr01@gmail.com>
2021-09-13 10:29:14 +0300

    calculating runtime (temporarily)

--- cubis/__main__.py
+++ cubis/__main__.py
@@ -1,5 +1,9 @@
 from src.cubis import main
-
+import time
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    end = time.time()
+
+    print("<*> Executed in {} seconds".format(end-start))
